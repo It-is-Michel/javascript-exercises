aa98d4693164664921586f122426080e5378cb83
Michel <221404137+It-is-Michel@users.noreply.github.com>
2025-08-09 12:28:37 -0300

    chore: Finish 9th exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+  // Args must be positives integers
+  if (first < 0
+      || last < 0
+      || !Number.isInteger(first)
+      || !Number.isInteger(last)
+  ) return "ERROR";
+  // If first greater than last, swap values
+  if (first > last) [first, last] = [last, first];
+  // first - last give us the difference, then we have to sum 1 to count the first number
+  const howManyNumbers = (last - first + 1);
+  return (first + last) * howManyNumbers / 2;
 };
 
 // Do not edit below this line
