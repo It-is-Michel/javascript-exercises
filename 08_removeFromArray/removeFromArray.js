bb7df40a9157c7af1e410048dcb510646a30eb0e
Michel <221404137+It-is-Michel@users.noreply.github.com>
2025-08-09 11:27:17 -0300

    chore: Finish 8th exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  for (const arg of args) {
+    while (true) {
+      const argIndex = array.findIndex(item => item === arg);
+      if (argIndex === -1) break;
+      array.splice(argIndex, 1);
+    };
+  };
+  return array
 };
 
 // Do not edit below this line
