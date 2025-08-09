64ed3df3bdeb38538da17bdb832b2a2b174825af
Michel <221404137+It-is-Michel@users.noreply.github.com>
2025-08-09 10:09:51 -0300

    chore: Finish 6th exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfCopies) {
+    if (numOfCopies < 0) return "ERROR";
+    
+    let result = "";
+    for (let i = 0; i < numOfCopies; i++) {
+        result += string;
+    };
+    return result;
 };
 
 // Do not edit below this line
