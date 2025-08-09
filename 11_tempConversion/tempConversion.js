f741353fd9a01c5894852fb5a61cada03aec51c6
Michel <221404137+It-is-Michel@users.noreply.github.com>
2025-08-09 13:39:50 -0300

    chore: Finish 11th exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegrees) {
+  const celsiusDegrees = (fahrenheitDegrees - 32) * 5 / 9;
+  const celsiusDegreesOneDecimal = Math.round(celsiusDegrees * 10) / 10;
+  return celsiusDegreesOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusDegrees) {
+  const fahrenheitDegrees = celciusDegrees * 9 / 5 + 32;
+  const fahrenheitDegreesOneDecimal = Math.round(fahrenheitDegrees * 10) / 10;
+  return fahrenheitDegreesOneDecimal;
 };
 
 // Do not edit below this line
